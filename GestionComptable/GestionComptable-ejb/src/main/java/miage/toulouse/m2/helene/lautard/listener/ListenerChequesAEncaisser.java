90b6c20a5d96893c22a1977d17cc8be968bf4266
HeleneLautard <helene.lautard@toulouse.miage.fr>
2021-11-20 16:47:03 +0100

    feat(Gestion Comptable): Ajout d'un listenner

--- GestionComptable/GestionComptable-ejb/src/main/java/miage/toulouse/m2/helene/lautard/listener/ListenerChequesAEncaisser.java
+++ GestionComptable/GestionComptable-ejb/src/main/java/miage/toulouse/m2/helene/lautard/listener/ListenerChequesAEncaisser.java
@@ -5,10 +5,14 @@
  */
 package miage.toulouse.m2.helene.lautard.listener;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 /**
  *
@@ -25,6 +29,16 @@ public class ListenerChequesAEncaisser implements MessageListener {
     @Override
     public void onMessage(Message message) {
         System.out.println("Message reçu - chèque à encaisser");
+        if(message instanceof TextMessage){
+            TextMessage msg = (TextMessage) message;
+            try {
+                System.out.println("Received : " + msg.getText());
+            } catch (JMSException ex) {
+                Logger.getLogger(ListenerChequesAEncaisser.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else if (message != null) {
+            System.out.println("Non Object Message Received");
+        }
     }
     
 }
