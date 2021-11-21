0f9f28675dd976243185bbfe39f394e86d80f693
HeleneLautard <helene.lautard@toulouse.miage.fr>
2021-11-21 10:52:55 +0100

    feat(Gestion Affaire): Ajout d'un sender
    Sender vers file de chèques à encaisser

---
+++ GestionAffaire/GestionAffaire-ejb/src/main/java/miage/toulouse/m2/helene/lautard/sender/SenderChequesAEncaisser.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miage.toulouse.m2.helene.lautard.sender;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author Hélène
+ */
+@MessageDriven(mappedName = "QUEUE_CHEQUES_A_ENCAISSER", activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class SenderChequesAEncaisser implements MessageListener {
+    
+    InitialContext context = null;
+    ConnectionFactory factory = null;
+    Connection connection = null;
+    String factoryName = "MenuisMiageConnectionFactory";
+    String destName = "QUEUE_CHEQUES_A_ENCAISSER";
+    Destination dest = null;
+    int count = 1;
+    Session session = null;
+    MessageProducer sender = null;
+    String text = "Message N° ";
+    
+    public SenderChequesAEncaisser() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+    }
+    
+    public void sendMsgChequesAEncaisser(){
+        try {
+            // create the JNDI initial context.
+            context = new InitialContext();
+
+            // look up the ConnectionFactory
+            factory = (ConnectionFactory) context.lookup(factoryName);
+
+            // look up the Destination
+            dest = (Destination) context.lookup(destName);
+
+            // create the connection
+            connection = factory.createConnection();
+
+            // create the session
+            session = connection.createSession(
+                false, Session.AUTO_ACKNOWLEDGE);
+
+            // create the sender
+            sender = session.createProducer(dest);
+
+            // start the connection, to enable message sends
+            connection.start();
+
+            for (int i = 0; i < count; ++i) {
+                TextMessage message  = session.createTextMessage();
+                message.setText(text + (i + 1));
+                sender.send(message);
+                System.out.println("Sent: " + message.getText());
+            }
+        } catch (JMSException exception) {
+            exception.printStackTrace();
+        } catch (NamingException exception) {
+            exception.printStackTrace();
+        } finally {
+            // close the context
+            if (context != null) {
+                try {
+                    context.close();
+                } catch (NamingException exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+            // close the connection
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
+    }
+}
